8ffcf451e870f2a41cfd79cda4b1307aeb4911f2
g0sj6f <g0sj6f@SZA0030900>
2022-11-30 11:08:04 +0100

    Add project files.

---
+++ projektf/Controllers/BoatController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace projektf.Controllers
+{
+    //[Route("api/[controller]")]
+    //[ApiController]
+    public class BoatController : ControllerBase
+    {
+        [HttpGet]
+        [Route("questions/all")]
+        public ActionResult M1()
+        {
+            Models.HajosContext context = new Models.HajosContext();
+            var kérdések = from x in context.Questions select x.Question1;
+
+            return new JsonResult(kérdések);
+
+        }
+    }
+}
---
+++ projektf/Program.cs
@@ -0,0 +1,48 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+//app.UseAuthorization();
+
+//app.MapRazorPages();
+
+//var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+
+//builder.Services.AddSwaggerGen();
+
+app.UseHttpsRedirection();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.MapControllers();
+
+app.Run();
+
+
+app.Run();
